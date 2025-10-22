db2924b10bb0f1ec7858314b251e793211527914
saurabh <svikram808@gmail.com>
2025-10-22 14:21:06 +0530

    first commit

---
+++ src/webparts/employeeDirectory/components/IEmployeeDirectoryProps.ts
@@ -0,0 +1,7 @@
+export interface IEmployeeDirectoryProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
